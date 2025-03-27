214abd545d381640086f0982ac98e8a8aeadd8ba
sakuto <shukatu.sakuto@gmail.com>
2025-03-27 21:23:19 +0900

    gmapはお金がかかるため、ここからはleafletを実装して地図アプリを作成していく

--- mapproject/map/views.py
+++ mapproject/map/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+# Create your views here.
+
 def index(request):
+    """
+    地図表示画面
+    """
+
     template = loader.get_template('map/index.html')
-    context = {}
-    return HttpResponse(template.render(context, request))
+    return HttpResponse(template.render(None, request))
