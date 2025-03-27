cdffbac8d03d5ebf79e566260e5058db6d79de52
sakuto <shukatu.sakuto@gmail.com>
2025-03-27 21:40:03 +0900

    leafletを使ったmapは一旦表示できた。これをしゅうごに渡していく

--- mapproject/map/static/map/app.js
+++ mapproject/map/static/map/app.js
@@ -1,1 +1,7 @@
-// leafletの処理追加
+// 座標とズームレベルを指定 例：東京
+const map = L.map('map').setView([35.681167, 139.767052], 10);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    // 右下にクレジットを表示
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
