6f8032c94d2701624740c195ab6dac617b581994
sakuto <shukatu.sakuto@gmail.com>
2025-03-29 14:02:42 +0900

    マップの新規作成,DBに保存、トップページに作成したマップ一覧を表示する機能までを追加した。

--- mapproject/map/urls.py
+++ mapproject/map/urls.py
@@ -5,5 +5,6 @@ app_name = 'map'
 urlpatterns = [
     path('', views.index, name='index'),
     # <map_id>対応ルーティングを追加
+    path('create/', views.create_map, name='create_map'),
     path('<str:map_id>/', views.map_view, name='map_view'),
 ]
